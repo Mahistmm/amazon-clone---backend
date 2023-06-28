59e2af62ac70b571d695cbb86cdd8187516ec61d
Mahistmm <118288798+Mahistmm@users.noreply.github.com>
2023-06-28 11:09:40 +0530

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+const { User } = require("../models/Index");
 
 function verifytoken(req, res, next) {
   const token = req.headers["authorization"];
